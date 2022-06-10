b80b15deb5ae543da5dc49b5d2b0b90a62e0c7c7
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2022-06-09 22:30:35 -0400

    tried new things

--- netlify/functions/degreeInfo.js
+++ netlify/functions/degreeInfo.js
@@ -1,26 +1,9 @@
+const degreeInfo = require('degreeInfo.json')
+
 exports.handler = async function () {
     return {
         statusCode: 200,
-        body: JSON.stringify({   "degrees": [
-                {
-                    "firstName": "Michael",
-                    "lastName": "Kramer",
-                    "studentID": "U17187772",
-                    "school": "Boston University",
-                    "program": "Software Development",
-                    "type" : "MS",
-                    "conferred": 2022
-                },
-                {
-                    "firstName": "Michael",
-                    "lastName": "Kramer",
-                    "studentID": "109345019",
-                    "school": "University of Maryland, College Park",
-                    "program": "Jazz Studies",
-                    "type" : "BA",
-                    "conferred": 2011
-                }
-            ] }),
+        body: JSON.stringify(degreeInfo),
         headers: {
             'Access-Control-Allow-Origin': 'https://kramer-bu-vue-json.netlify.app/',
             'Access-Control-Allow-Methods': 'GET',
