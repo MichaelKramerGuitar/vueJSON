43d06e6091a030f33bf031dc5541e91bdf880537
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2022-06-10 13:45:30 -0400

    tried adding 'context' to degrees.js

--- functions/degrees.js
+++ functions/degrees.js
@@ -2,8 +2,8 @@
 
 const degreeInfo = require('./data/degreeInfo.json');
 
-exports.handler = async (event) => {
-    await (event)
+exports.handler = async (event, context) => {
+    await (event, context)
     return {
         statusCode: 200,
         body: JSON.stringify(degreeInfo.degrees)
